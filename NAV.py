8ea3db8592a9c5d0ca9a463758466f7ee62843e6
Doug Swain <dswain@gmail.com>
2016-01-01 17:28:00 -0500

    Create a basic implementation of __gt__ to get our code closer to working completely.
    We only compare NAV's that share a ticker. Otherwise, we raise a NotImplemented exception. The rest is the work of the Real classes __gt__ method.

--- NAV.py
+++ NAV.py
@@ -7,4 +7,16 @@ class NAV:
         self.ticker = ticker
         self.price = price
 
-    
+    """
+     Okay, so now we know we want to implement the greater-than operator (>). Let's do it!
+     We'll only compare the same ticker to itself. Otherwise, we'll say that this isn't implemented.
+     
+     One nice feature in this implementation - We just delegated the real work to the Real implementation
+      of __gt__ (https://docs.python.org/3.5/library/numbers.html#numbers.Real)
+    """
+    def __gt__(self, other):
+        if self.ticker == other.ticker:
+            return self.price > other.price
+        else:
+            return NotImplemented
+
