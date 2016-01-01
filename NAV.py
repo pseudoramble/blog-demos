7783845faac18aecc80662cfd92ab58b18633eb1
Doug Swain <dswain@gmail.com>
2016-01-01 17:11:09 -0500

    Initial commit. Added in an intentionally broken NAV class with some code that we want to get working.
    Note that I am assuming I didn't put a bug into the code we want to get working either.

---
+++ NAV.py
@@ -0,0 +1,4 @@
+class NAV:
+    def __init__(self, ticker, price):
+        self.ticker = ticker
+        self.price = price
---
+++ main.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+from NAV import NAV
+
+def navOf(ticker, date):
+    if date and date.year > 2014:
+        return NAV(ticker, 71.80)
+    else:
+        return NAV(ticker, 70.87)
+
+if __name__ == '__main__':
+    ticker = 'FUSEX'
+    nav_2014 = navOf(ticker, date(2014, 12, 31))
+    nav_2015 = navOf(ticker, date(2015, 12, 31))
+
+    print('For the ticker %s:' % ticker)
+    
+    if nav_2014 > nav_2015:
+        print('\tAt a NAV of %s (versus %s), 2014 was a better year than 2015' % (nav_2014, nav_2015))
+    else:
+        print('\tAt a NAV of %s (versus %s), 2015 was a better year than 2014' % (nav_2015, nav_2014))
