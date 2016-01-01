a3d0356d17d2e2d7e4299ce462d1a6845d3d0ae0
Doug Swain <dswain@gmail.com>
2016-01-01 17:14:41 -0500

    Added a comment to __init__

--- NAV.py
+++ NAV.py
@@ -1,4 +1,10 @@
 class NAV:
+    """
+     Our first example of a Python built-in. This should be familiar if you've implemented a class before.
+     This completes some of what a constructor would provide in other languages.
+    """
     def __init__(self, ticker, price):
         self.ticker = ticker
         self.price = price
+
+    
